62e8311d62fd1fd770c2a5064407ef0084475836
Spencer Allegrezza <sallegrezz@gmail.com>
2018-12-01 20:44:55 -0600

    Add progress bar to renderer. Possibly not to stay

--- src/app/content/renderer/renderer.component.ts
+++ src/app/content/renderer/renderer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RendererComponent implements OnInit {
   @Input() body: string;
+  @Input() progress: number = 50;
   constructor() { }
 
   ngOnInit() {
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { CategoryItem } from './categories'
+import { HttpClient, HttpRequest, HttpEvent, HttpEventType } from '@angular/common/http';
+import { CategoryItem } from './categories';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 
@@ -9,11 +9,35 @@ import { environment } from '../environments/environment';
 })
 export class RestService {
   constructor(private http: HttpClient) {}
-  API = `http://${environment.api}:3000`
-  getPage(path: string){
-    return this.http.get(`${this.API}/${path}`)
+  API = `http://${environment.api}:3000`;
+
+  private getEventMessage(event: HttpEvent<any>, file: File) {
+    switch (event.type) {
+      case HttpEventType.Sent:
+        return `Uploading file "${file.name}" of size ${file.size}.`;
+
+      case HttpEventType.UploadProgress:
+        // Compute and show the % done:
+        const percentDone = Math.round((100 * event.loaded) / event.total);
+        return `File "${file.name}" is ${percentDone}% uploaded.`;
+
+      case HttpEventType.Response:
+        return `File "${file.name}" was completely uploaded!`;
+
+      default:
+        return `File "${file.name}" surprising upload event: ${event.type}.`;
+    }
+  }
+  getPage(path: string) {
+
+      return this.http.get(`${this.API}/${path}`)
+
+    // const req = new HttpRequest('GET', `${this.API}/${path}`, {
+    //   reportProgress: true
+    // });
+    // return this.http.request(req).pipe(map(event => this.getEventMessage(event, file)));
   }
   getDirectory(): Observable<any> {
-   return this.http.get(`${this.API}`);
+    return this.http.get(`${this.API}`);
   }
 }
